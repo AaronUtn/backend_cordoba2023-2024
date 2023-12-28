518fdb91aadb7cbc7f2c3b9f149cc2cf50c1d4c8
AaronUtn <aariassolorza@frba.utn.edu.ar>
2023-12-28 16:06:33 -0300

    agregadoAntesDel2812

---
+++ clase2/un_ejemplo_exportar/appEj2.js
@@ -0,0 +1,4 @@
+const {sumar,multiplicar} = require('./calculadora');
+
+console.log(sumar(2,3));
+console.log(multiplicar(2,3));
---
+++ clase2/un_ejemplo_exportar/calculadora.js
@@ -0,0 +1,14 @@
+const sumar = (numeroUno,numeroDos) =>{
+
+    return numeroUno + numeroDos;
+};
+
+const multiplicar = (numeroUno,numeroDos) =>{
+
+    return numeroUno * numeroDos;
+};
+
+//console.log("suma :" + sumar(4,5));
+
+//para exportar el modulo
+module.exports = {sumar,multiplicar};
